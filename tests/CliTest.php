8c118304f31c11b303e966ef04d921f8a11be5a6
deeem <karpov.dimon@gmail.com>
2016-08-04 12:25:43 +0300

    Add rules to phpunit cli tests

--- tests/CliTest.php
+++ tests/CliTest.php
@@ -4,8 +4,37 @@ namespace PsrLinter;
 
 class CliTest extends \PHPUnit\Framework\TestCase
 {
-    public function testSingleFile()
+    public function testNoArguments()
     {
-        $this->assertEquals('', exec('./bin/hexlet-psr-linter tests/code.php'));
+        $result = exec('./bin/psr-linter');
+        $this->assertEquals('Usage: psr-linter filename', $result);
+    }
+
+    public function testPathNotExists()
+    {
+        $result = exec('./bin/psr-linter code.php');
+        $this->assertEquals('Usage: psr-linter filename', $result);
+    }
+
+    public function testFileValid()
+    {
+        exec('./bin/psr-linter tests/snippets/valid.php', $output, $return_var);
+        $this->assertEquals(0, $return_var);
+    }
+
+    public function testFileNotValid()
+    {
+        $descriptorspec = array(
+            0 => array("pipe", "r"),
+            1 => array("pipe", "w"),
+            2 => array("pipe", "w")
+        );
+
+        $cmd = './bin/psr-linter tests/snippets/functionsNaming.php';
+        $process = proc_open($cmd, $descriptorspec, $pipes, null, null);
+        $stderr = stream_get_contents($pipes[2]);
+        fclose($pipes[2]);
+        proc_close($process);
+        $this->assertFalse(empty($stderr));
     }
 }
