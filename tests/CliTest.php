dae48fcc817e106f10f3d009377da88b6813c9ab
deeem <karpov.dimon@gmail.com>
2016-08-05 22:31:38 +0300

    Change argv method for using Commando

--- tests/CliTest.php
+++ tests/CliTest.php
@@ -4,37 +4,14 @@ namespace PsrLinter;
 
 class CliTest extends \PHPUnit\Framework\TestCase
 {
-    public function testNoArguments()
+    public function testInvalidArguments()
     {
-        $result = exec('./bin/psr-linter');
-        $this->assertEquals('Usage: psr-linter filename', $result);
+        $this->assertEquals(1, preg_match("/ERROR/", exec('./bin/psr-linter')));
     }
 
-    public function testPathNotExists()
-    {
-        $result = exec('./bin/psr-linter code.php');
-        $this->assertEquals('Usage: psr-linter filename', $result);
-    }
-
-    public function testFileValid()
+    public function testCheckValidFile()
     {
         exec('./bin/psr-linter tests/snippets/valid.php', $output, $return_var);
         $this->assertEquals(0, $return_var);
-    }
-
-    public function testFileNotValid()
-    {
-        $descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("pipe", "w")
-        );
-
-        $cmd = './bin/psr-linter tests/snippets/functionsNaming.wrong.1.php';
-        $process = proc_open($cmd, $descriptorspec, $pipes, null, null);
-        $stderr = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
-        proc_close($process);
-        $this->assertFalse(empty($stderr));
     }
 }
