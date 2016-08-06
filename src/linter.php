39d8d2d4cf58522f9db6ebfbad9ddb3fde5cc7f3
deeem <karpov.dimon@gmail.com>
2016-08-06 20:46:30 +0300

    Refactor Linter class

---
+++ src/linter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PsrLinter;
+
+use \PhpParser\ParserFactory;
+use \PhpParser\Error;
+use \PhpParser\NodeTraverser;
+use PsrLinter\NodeVisitor;
+use PsrLinter\ExceptionParse;
+
+
+function linter($code)
+{
+    $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+    $visitor = new NodeVisitor();
+    $traverser = new \PhpParser\NodeTraverser;
+    $traverser->addVisitor($visitor);
+
+    try {
+        $stmts = $parser->parse($code);
+        $traverser->traverse($stmts);
+    } catch (\PhpParser\Error $e) {
+        throw new ExceptionParse();
+    }
+
+    return $visitor->getErrors();
+}
+
+//class Linter
+//{
+//    public function validate($code)
+//    {
+//        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+//        $visitor = new NodeVisitor();
+//        $traverser = new \PhpParser\NodeTraverser;
+//        $traverser->addVisitor($visitor);
+//
+//        try {
+//            $stmts = $parser->parse($code);
+//            $traverser->traverse($stmts);
+//        } catch (\PhpParser\Error $e) {
+//            throw new ExceptionParse();
+//        }
+//
+//        return $visitor->getErrors();
+//    }
+//}
