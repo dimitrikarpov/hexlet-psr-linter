08990eb5bb4d0dd103d2e1fe3b400ebd4c13beb4
deeem <karpov.dimon@gmail.com>
2016-08-11 09:08:59 +0300

    Fix tests

--- tests/CheckersTest.php
+++ tests/CheckersTest.php
@@ -15,64 +15,78 @@ class CheckersTest extends \PHPUnit\Framework\TestCase
 
     public function testFunctionsNamingWrong()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/functionsNaming.wrong.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse(empty($errors));
     }
 
     public function testFunctionsNamingRight()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/functionsNaming.right.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse($errors);
     }
 
     public function testVariablesNamingWrong()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/functionsNaming.wrong.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse(empty($errors));
     }
 
     public function testVariablesNamingRight()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/variablesNaming.right.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse($errors);
     }
 
     public function testDeclarationsOnlyFile()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/declarations.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse($errors);
     }
 
     public function testSideEffectsOnlyFile()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/sideEffects.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+
         $this->assertFalse($errors);
     }
 
     public function testDeclarationsAndSideEffectsContainingFile()
     {
-        $lint = $this->lint;
-
         $code = file_get_contents('tests/fixtures/declarationsAndSideEffects.php');
-        $errors = $lint($code);
+
+        $lint = $this->lint;
+        $linterReport = $lint($code);
+        $errors = $linterReport['errors'];
+        
         $this->assertFalse(empty($errors));
     }
 }
